0b9cf43451f77146f06ad0a1c28b1e4121417c53
Shelly Yusuf <ShellyNYusuf@gmail.com>
2017-12-05 13:08:10 -0800

    more travis db fun

--- test/bookTest.js
+++ test/bookTest.js
@@ -7,7 +7,6 @@ const expect = chai.expect;
 const chaiHTTP = require('chai-http')
 chai.use(chaiHTTP)
 const request= chai.request
-
 const app = require(__dirname + '/../server/_server');
 const port = 4000;
 
@@ -49,7 +48,7 @@ it('should get the book route', (done)=>{
   request('http://localhost:4000')
   .get('/api/books')
   .end((err, res)=>{
-    console.log(res.body);
+    // console.log(res.body);
     console.log(res.status);
     if(err){
       console.log(err);
@@ -64,7 +63,7 @@ it('should post to the book db', (done)=>{
   .send({user_id: 1, title:'Test Title ', author: 'Test Author 2', currentlyWith: 'Test me 2', ownerEmail:'test@test.com', available: 'True'})
   .end((err, res)=>{
     console.log(res.status);
-    console.log(res.body);
+    // console.log(res.body);
     expect(err).to.eql(null)
     expect(res.status).to.eql(200)
     done()
